c452c3d125920b5cca571d2678a9a60fe59661ff
David Young-Chan Kay <davidykay@gmail.com>
2015-04-26 01:18:47 -0400

    deleted unused screens. added emotionalCheckinVC but no content

---
+++ ios/CancerNavigator/EmotionalCheckinViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EmotionalCheckinViewController.swift
+//  CancerNavigator
+//
+//  Created by David Young-chan Kay on 4/26/15.
+//  Copyright (c) 2015 David Young-chan Kay. All rights reserved.
+//
+
+import Foundation
