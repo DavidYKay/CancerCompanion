5fc6ed592bfe44c9845fd9669b0757b447a7f76b
John Tan <jctan00@gmail.com>
2015-04-26 08:43:17 -0400

    added cell selected rows under tableView for checklistView

--- CancerNavigator/ChecklistViewController.swift
+++ CancerNavigator/ChecklistViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 
 public class ChecklistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     
+    
     @IBOutlet
     var tableView: UITableView!
     var items: [String] = ["1. What is my diagnosis & stage?",
@@ -18,10 +19,17 @@ public class ChecklistViewController: UIViewController, UITableViewDelegate, UIT
                            "4. What might happen if I decide not to have treatment?",
                            "5. Do I need to decide right away, or can I wait until after holiday or vacation?"]
     
+    let cellIdentifier = "StandardCellId"
+    var lastSelectedIndexPath: NSIndexPath? = nil
+    
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
+        
         self.navigationItem.title = "Checklist"
-//        self.tableView.registerClass(UITableViewCell.self,forCellReuseIdentifier: "cell")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .Plain, target: self, action: "onDone")
+        self.tableView.registerClass(UITableViewCell.self,forCellReuseIdentifier: cellIdentifier)
+        self.tableView.allowsMultipleSelection = true
     }
 
     func onDone() {
@@ -36,23 +44,70 @@ public class ChecklistViewController: UIViewController, UITableViewDelegate, UIT
         
 //        let myData = myArray[indexPath.row]
 //        cellIdentifier = myData ? "a" : "b"
-        let cellIdentifier = "StandardCellId"
+        //let cellIdentifier = "StandardCellId"
         
-        var cell:UITableViewCell? = self.tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+        
+        var cell:UITableViewCell? = self.tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as? UITableViewCell
+        cell!.textLabel?.text = self.items[indexPath.row]
+        //cell!.accessoryType = (self.lastSelectedIndexPath?.row == indexPath.row) ? .Checkmark : .None
         
         if(cell == nil){
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIdentifier)
-        } else{
+        }
+        //else{
             // we already have one from reuse
+        //}
+        
+        if let selectedPaths = tableView.indexPathsForSelectedRows() as? [NSIndexPath] {
+            let selected = selectedPaths.filter(){ $0 == indexPath }
+            if selected.count > 0 {
+                cell!.accessoryType = (self.lastSelectedIndexPath?.row == indexPath.row) ? .Checkmark :.None
+            }
         }
         
-        cell!.textLabel?.text = self.items[indexPath.row]
-        
+        cell!.backgroundColor = UIColor.clearColor()
+        cell!.textLabel?.textColor = UIColor.darkTextColor()
+        cell!.selectionStyle = UITableViewCellSelectionStyle.Blue
         return cell!
     }
     
     public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         println("You selected cell #\(indexPath.row)!")
+        
+        //tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+        
+        if indexPath.row != self.lastSelectedIndexPath?.row {
+            if let lastSelectedIndexPath = lastSelectedIndexPath {
+                let oldCell = tableView.cellForRowAtIndexPath(lastSelectedIndexPath)
+                oldCell?.accessoryType = .None
+            }
+            
+            //var cell:UITableViewCell? = self.tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as? UITableViewCell
+            
+            var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIdentifier)
+            //cell!.textLabel?.text = self.items[indexPath.row]
+            //cell!.accessoryType = (self.lastSelectedIndexPath?.row == indexPath.row) ? .Checkmark : .None
+            
+            if let selectedPaths = tableView.indexPathsForSelectedRows() as? [NSIndexPath] {
+                let selected = selectedPaths.filter(){ $0 == indexPath }
+                if selected.count > 0 {
+                     cell.accessoryType = (self.lastSelectedIndexPath?.row == indexPath.row) ? .Checkmark :.None
+                }
+            }
+
+            lastSelectedIndexPath = indexPath
+        }
+        //tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.None)
+        //tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+    }
+//
+    public func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+          let newCell = tableView.cellForRowAtIndexPath(indexPath)
+          newCell?.accessoryType = .None
+
     }
 
 
